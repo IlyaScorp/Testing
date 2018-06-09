3577a668a265a80b44888a4797bf589f37c1e83f
IlyaScorp <ilyabelan7cc@mail.ru>
2018-06-09 21:21:50 +0300

    hw8

--- src/main/java/com/spbstu/enums/elements_page/OVOSHI.java
+++ src/main/java/com/spbstu/enums/elements_page/OVOSHI.java
@@ -2,6 +2,11 @@ package com.spbstu.enums.elements_page;
 
 public enum OVOSHI {
 
+    //TODO Решил сделать енамы в таком видое. Жду рекомендаций почему так плохо и чаще встречается
+    //TODO как в com.spbstu.JDIPages.sections.enums.ELEMENTS. К чему писать это лишний код
+    //TODO для реализации конструкторов, если можно сделать как здесь.
+
+    //TODO вижу это актуаьным только когда названием енамов отличается от текста.
     Cucumber,
     Tomato,
     Salad,
